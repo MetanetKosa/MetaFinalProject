47095b512d5cc40dbb4d0da6bf685e10ee85a266
seeun jeong <sesej4512@gmail.com>
2023-04-14 14:25:13 +0900

    Feat: add MypageMapper, MypageService, MypageMapper test

---
+++ src/main/java/com/example/metawater/service/MypageService.java
@@ -0,0 +1,23 @@
+package com.example.metawater.service;
+
+import com.example.metawater.domain.ProductVO;
+import com.example.metawater.mapper.MypageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MypageService {
+    @Autowired
+    private MypageMapper mypageMapper;
+
+    public List<ProductVO> getMyProductList(Long memNo) {
+            return mypageMapper.getMyProductList(memNo);
+        }
+
+    public List<ProductVO> getMyOrderList() {
+        return mypageMapper.getMyOrderList();
+        }
+
+    }
