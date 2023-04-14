8111325adcfa152a1e9d378132e087045041d431
eunyoung56 <eunyeong1328@daum.net>
2023-04-14 18:58:42 +0900

    Feat: add SecurityConfig

--- src/main/java/com/example/metawater/security/SecurityConfig.java
+++ src/main/java/com/example/metawater/security/SecurityConfig.java
@@ -15,19 +15,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //    private final MemberService memberService;
 //
 //
-//    public SecurityConfig(MemberService memberService) {
-//        this.memberService = memberService;
-//    }
-//
-//    /**
-//     * 규칙 설정
-//     * @param http
-//     * @throws Exception
-//     */
-//    protected void configure(HttpSecurity http) throws Exception{
-//        http.authorizeRequests()
-//                .antMatchers("/login","signup","/resources/**").permitAll()//로그인 권한은 누구나, resources파일도 모든권한
-//                //User,Admin 접근 허용
+////    public SecurityConfig(MemberService memberService) {
+////        this.memberService = memberService;
+////    }
+////
+////    /**
+////     * 규칙 설정
+////     * @param http
+////     * @throws Exception
+////     */
+////    protected void configure(HttpSecurity http) throws Exception{
+////        http.authorizeRequests()
+////                .antMatchers("/login","signup","/resources/**").permitAll()//로그인 권한은 누구나, resources파일도 모든권한
+////                //User,Admin 접근 허용
 //                .antMatchers("/userAccess").hasRole("ROLE_MEMBER")
 //                .antMatchers("/userAccess").hasRole("ROLE_ADMIN")
 //                .and()
