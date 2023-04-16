462d28b2cb8731b7031c9b16f3f91011cb0556ae
ssohee98 <jsohee98@naver.com>
2023-04-17 01:48:38 +0900

    Fix: ProductInq ing..

--- src/main/java/com/example/metawater/controller/ProductInqController.java
+++ src/main/java/com/example/metawater/controller/ProductInqController.java
@@ -1,10 +1,7 @@
 package com.example.metawater.controller;
 
-import com.example.metawater.domain.ProductQVO;
-import com.example.metawater.domain.ProductVO;
-import com.example.metawater.domain.ReviewVO;
-import com.example.metawater.service.ProductQService;
-import com.example.metawater.service.ReviewService;
+import com.example.metawater.domain.ProductInqVO;
+import com.example.metawater.service.ProductInqService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,31 +11,31 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/product")
-public class ProductQController {
+public class ProductInqController {
 
     @Autowired
-    private ProductQService pqService;
+    private ProductInqService pqService;
 
-    public ProductQController(ProductQService pqService) {this.pqService = pqService;}
+    public ProductInqController(ProductInqService pqService) {this.pqService = pqService;}
 
     //문의 목록 조회
     @GetMapping("/{productNo}/proQna")
-    public List<ProductQVO> list(@PathVariable Long productNo) {
+    public List<ProductInqVO> list(@PathVariable Long productNo) {
         System.out.println("문의 GET 요청 확인");
         return pqService.findByProduct(productNo);
     }
 
     //문의 상세 조회
     @GetMapping("/{productNo}/proQna/detail/{proqNo}")
-    public ProductQVO detail(@PathVariable Long productNo, @PathVariable Long proqNo) {
+    public ProductInqVO detail(@PathVariable Long productNo, @PathVariable Long proqNo) {
         return pqService.getQuestion(productNo, proqNo);
     }
 
     //문의 등록
     @PostMapping("/{productNo}/qnaInsert")
-    public ResponseEntity insert(@PathVariable Long productNo, @RequestBody ProductQVO question) {
+    public ResponseEntity insert(@PathVariable Long productNo, @RequestBody ProductInqVO question) {
         question.setProductNo(productNo);
-        pqService.insertQuestion(question);
+        pqService.insertQuestion(productNo, question);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
--- src/main/java/com/example/metawater/domain/ProductInqVO.java
+++ src/main/java/com/example/metawater/domain/ProductInqVO.java
@@ -1,19 +1,22 @@
 package com.example.metawater.domain;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
-public class ProductQVO {
+public class ProductInqVO {
     private Long proqNo;
     private Long memNo;
     private Long productNo;
     private String proqTitle;
     private String proqContent;
-    private Date progDate;
-    private String progState;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date proqDate;
+    private String proqState;
     //답변
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date answerDate;
     private String answerContent;
     //이메일 수신 여부
