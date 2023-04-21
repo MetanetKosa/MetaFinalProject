eebe829f2c18a9f78f34383239af56e91a924934
eunyoung56 <eunyeong1328@daum.net>
2023-04-21 14:44:48 +0900

    FIX:fix login2 and inqMapper

--- src/main/java/com/example/metawater/controller/ProductInqController.java
+++ src/main/java/com/example/metawater/controller/ProductInqController.java
@@ -27,13 +27,17 @@ public class ProductInqController {
 //        System.out.println("문의 GET 요청 확인");
 //        return pqService.findByProduct(productNo);
 //    }
-    public ResponseEntity<List<ProductInqVO>> list(@PathVariable Long productNo) {
+//    public ResponseEntity<List<ProductInqVO>> list(@PathVariable Long productNo) {
+
+        public List<ProductInqVO> list(@PathVariable Long productNo) {
         System.out.println("문의 GET 요청 확인");
         System.out.println("productNon데이터 확인" + productNo);
 
-        System.out.println("----------"+pqService.findByProduct(productNo));
+        System.out.println("-----문의 GET 요청을 이렇게 보내도 되나요?-----" + pqService.findByProduct(productNo));
+        List<ProductInqVO> productList = pqService.findByProduct(productNo);
+        return productList;
+//        return new ResponseEntity<>(pqService.findByProduct(productNo), HttpStatus.OK);
 
-        return new ResponseEntity<>(pqService.findByProduct(productNo), HttpStatus.OK);
     }
 
     //문의 상세 조회
